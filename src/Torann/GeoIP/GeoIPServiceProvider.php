7261f057d22e480194bc12a96ad720886fefecee
Steven Barnett <steven@volarvideo.com>
2015-02-24 13:36:09 -0500

    Tagged publishing did not work despite following Laravel docs. Trying standard publishing.

--- src/Torann/GeoIP/GeoIPServiceProvider.php
+++ src/Torann/GeoIP/GeoIPServiceProvider.php
@@ -20,11 +20,8 @@ class GeoIPServiceProvider extends ServiceProvider {
 	{
 		$this->publishes([
 			__DIR__.'/../../config/geoip.php' => config_path('geoip.php'),
-		], 'config');
-		
-		$this->publishes([
 			__DIR__.'/../../migrations/' => base_path('/database/migrations'),
-		], 'migrations');
+		]);
 	}
 
 	/**
