5c0d5a884bfa8c1e4b089fbdc5c97b3623f99599
Daniel Stainback <torann@gmail.com>
2017-01-26 11:59:54 -0500

    Add support for Laravel 5.4

--- src/Torann/GeoIP/GeoIPServiceProvider.php
+++ src/Torann/GeoIP/GeoIPServiceProvider.php
@@ -5,53 +5,51 @@ use Torann\GeoIP\Console\UpdateCommand;
 
 class GeoIPServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->publishes([
-			__DIR__.'/../../config/geoip.php' => config_path('geoip.php'),
-		]);
-	}
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../../config/geoip.php' => config_path('geoip.php'),
+        ]);
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		// Register providers.
-		$this->app['geoip'] = $this->app->share(function($app)
-		{
-			return new GeoIP($app['config'], $app["session.store"]);
-		});
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('geoip', function ($app) {
+            return new GeoIP($app['config'], $app['session.store']);
+        });
 
-		$this->app['command.geoip.update'] = $this->app->share(function ($app)
-		{
-			return new UpdateCommand($app['config']);
-		});
-		$this->commands(['command.geoip.update']);
-	}
+        $this->app->singleton('command.geoip.update', function ($app) {
+            return new UpdateCommand($app['config']);
+        });
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('geoip', 'command.geoip.update');
-	}
+        $this->commands(['command.geoip.update']);
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('geoip', 'command.geoip.update');
+    }
 
 }
