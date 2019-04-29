8d48b1d86dec11416b1cee06f8a852f66556ace7
benemohamed <49129795+benemohamed@users.noreply.github.com>
2019-04-29 21:57:06 +0100

    Added support for ipfinder.io

---
+++ src/Services/IPFinder.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Torann\GeoIP\Services;
+
+use Exception;
+use Illuminate\Support\Arr;
+use Torann\GeoIP\Support\HttpClient;
+
+/**
+ * Class GeoIP
+ * @package Torann\GeoIP\Services
+ */
+class IPFinder extends AbstractService
+{
+    /**
+     * Http client instance.
+     *
+     * @var HttpClient
+     */
+    protected $client;
+
+    /**
+     * The "booting" method of the service.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->client = new HttpClient([
+            'base_uri' => 'https://api.ipfinder.io/v1/',
+            'headers' => [
+                'User-Agent' => 'Laravel-GeoIP-Torann',
+            ],
+            'query'    => [
+                'token' => $this->config('key'),
+            ],
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @throws Exception
+     */
+    public function locate($ip)
+    {
+        // Get data from client
+        $data = $this->client->get($ip);
+
+        // Verify server response
+        if ($this->client->getErrors() !== null || empty($data[0])) {
+            throw new Exception('Request failed (' . $this->client->getErrors() . ')');
+        }
+
+        $json = json_decode($data[0], true);
+
+        return $this->hydrate($json);
+    }
+}
